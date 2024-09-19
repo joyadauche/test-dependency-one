3af90a73d7aa24d6b40e9a97b1a58ca44d81aa0e
Joy Uchenna <joy.adaeze.uchenna@gmail.com>
2024-09-19 17:05:14 -0400

    Add dog dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/joyadauche/test-dependency-one
 
 go 1.23.1
+
+require github.com/joyadauche/test-dependency-two v0.0.0-20240919205344-d02cdeab8de5
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	dog "github.com/joyadauche/test-dependency-two"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarsk() string {
+	return dog.WhenGrownUp(Barks())
 }
